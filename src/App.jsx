a9d97cf2a0426c63543bc768d97060a593468e94
YuliannC <yuliannaperez1707@gmail.com>
2023-02-15 16:45:21 -0500

    actualizacion5

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 function App() {
+  const [url, SetUrl] = useState('https://localhost:7210/api/t_usuarios');
+  const Mostrar = () =>{
+    axios.get(url).then(Response=>{
+      console.log(Response.Data);
+    })
+  }
 
   return (
           <div>
-              <button className="boton">Registrar</button> <br /> <br />
+              <button className="boton" onClick={ Mostrar }>Registrar</button> <br /> <br />
           <table>
     <thead>
     <tr>
@@ -19,7 +25,6 @@ function App() {
     <th scope="col">Tipo de documento</th>
     <th scope="col">Numero de documento</th>
     <th scope="col">Telefono</th>
-    <th scope="col">Correo</th>
     <th scope="col"></th>
     <th scope="col"></th>
 
@@ -33,13 +38,11 @@ function App() {
     <td></td>
     <td></td>
     <td></td>
-    <td></td>
     <td><button className="boton">Editar</button></td>
     <td><button className="boton">Eliminar</button></td>
     </tr>
     <tr>
     <th scope="row"></th>
-    <td></td>
     <td></td>
     <td></td>
     <td></td>
@@ -56,12 +59,12 @@ function App() {
     <td></td>
     <td></td>
     <td></td>
-    <td></td>
     <td><button className="boton">Editar</button></td>
     <td><button className="boton">Eliminar</button></td>
     </tr>
     </tbody>
     </table>
+   
 </div>
   );
   
