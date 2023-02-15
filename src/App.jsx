409446ca964c65f9e416f34fcd645a7d49a3b345
YuliannC <yuliannaperez1707@gmail.com>
2023-02-15 16:14:38 -0500

    actualizacion4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import ApiCrud from './componentes/ApiCrud'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
 
