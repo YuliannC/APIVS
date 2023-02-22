05ad10e9dba2106e6ce6fd5f6db41bad105ff946
YuliannC <yuliannaperez1707@gmail.com>
2023-02-22 15:14:58 -0500

    yulianna actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import ApiCrud from './componentes/ApiCrud'
@@ -6,12 +6,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 function App() {
+
   const [url, SetUrl] = useState('https://localhost:7210/api/t_usuarios');
   const Mostrar = () =>{
     axios.get(url).then(Response=>{
       console.log(Response.Data);
     })
   }
+
+  const CargarDatos = async()=>{
+    await axios.get('https://localhost:7210/api/t_usuarios').then(Response=>{
+      console.log(Response.data);
+    });
+  }
+  useEffect(()=>{
+    CargarDatos();
+  },[])
 
   return (
           <div>
@@ -27,43 +37,27 @@ function App() {
     <th scope="col">Telefono</th>
     <th scope="col"></th>
     <th scope="col"></th>
-
     </tr>
     </thead>
     <tbody>
-    <tr>
-    <th scope="row"></th>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td><button className="boton">Editar</button></td>
-    <td><button className="boton">Eliminar</button></td>
-    </tr>
-    <tr>
-    <th scope="row"></th>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td><button className="boton">Editar</button></td>
-    <td><button className="boton">Eliminar</button></td>
-    </tr>
+    {data.map(CargarDatos=>(
+          <tr key={CargarDatos.id}>
+            <td>{CargarDatos.id}</td>
+            <td>{CargarDatos.nombres}</td>
+            <td>{CargarDatos.apellidos}</td>
+            <td>{CargarDatos.tipodocumentoid}</td>
+            <td>{CargarDatos.numdocumento}</td>
+            <td>{CargarDatos.telefono}</td>
+            <td>
+              <button className="btn btn-primary" onClick={()=>seleccionarGestor(gestor, "Editar")}>Editar</button> {"  "}
+              <button className="btn btn-danger" onClick={()=>seleccionarGestor(gestor, "Eliminar")}>Eliminar</button>
+            </td>
+            </tr>
+        ))}
+        </tbody>
 
-    <tr>
-    <th scope="row"></th>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td><button className="boton">Editar</button></td>
-    <td><button className="boton">Eliminar</button></td>
-    </tr>
-    </tbody>
-    </table>
+      </table>
+
    
 </div>
   );
